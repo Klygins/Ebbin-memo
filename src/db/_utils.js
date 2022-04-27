a54e64cc59810010e39fcc35759cad0e628974a6
Andrey Klygin <gfka63@gmail.com>
2022-04-28 01:48:55 +0700

    search memo

--- src/db/_utils.js
+++ src/db/_utils.js
@@ -2,7 +2,7 @@ export function calculateNextShowTime(startTime, showedAmount) {
     const minutes = startTime + 1000 * 60
     const hours = startTime + 1000 * 3600
     const days = startTime + 1000 * 3600 * 24
-    switch (showedAmount){
+    switch (showedAmount) {
         case 0:
             return minutes * 10     // 10 minutes
         case 1:
@@ -18,4 +18,27 @@ export function calculateNextShowTime(startTime, showedAmount) {
         case 6:
             return days * 30 * 4    // 4 month
     }
+}
+
+export function compareWithTagsAndFolder(memos = [], tags, folder) {
+    const resultMemos = []
+
+    memos.forEach(memo => {
+        var folderOkey = false, tagsOkey = false
+        if (!folder) folderOkey = true
+        if (tags.length === 0) tagsOkey = true
+
+        if (folder && memo.folder === folder)
+            folderOkey = true
+
+        for (let index = 0; index < tags.length; index++) {
+            const tag = tags[index]
+            if (memo.tags.includes(tag)) {
+                tagsOkey = true; break
+            }
+        }
+        if (folderOkey && tagsOkey) resultMemos.push(memo)
+    })
+
+    return resultMemos
 }
